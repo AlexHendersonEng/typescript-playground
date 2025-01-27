002e59341f836388b11f75fe9e1ba72410cfc388
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-27 21:52:55 +0000

    Updated interfaces project

--- interfaces/main.ts
+++ interfaces/main.ts
@@ -2,13 +2,16 @@
 interface Person {
   name: string;
   age: number;
+  hello: () => void;
 }
 
 // Create object
 const person: Person = {
   name: 'John',
-  age: 30
+  age: 30,
+  hello: function() { console.log('Hello', this.name) }
 };
 
 // Print object
 console.log(person);
+person.hello();
