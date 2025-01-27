884b46f58fe0d4ec46045a037c05a5ecb1234404
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-27 19:34:12 +0000

    Added optional_chaining project

---
+++ optional_chaining/main.ts
@@ -0,0 +1,37 @@
+/*
+    Optional chaining: A process which allows the access of nested properties of an object
+                       without the need to validate the existence of each property in the chain.
+                       It first checks the existence of the property and then accesses it. If the
+                       variable is null or undefined, it returns undefined.
+*/
+
+// Define User type
+type User = { 
+    id: number; 
+    name?: { 
+        firstName: string; 
+        lastName?: string; 
+    } 
+}; 
+
+// Define array of User type
+const users: User[] = [{ 
+    id: 1, 
+    name: { 
+        firstName: "John",
+    } 
+}, 
+{ 
+    id: 2, 
+    name: { 
+        firstName: "Fred", 
+        lastName: "Doe"
+    } 
+}, 
+{ 
+    id: 3 
+}, 
+];
+
+// Use map to print out the last name of each user
+users.map(element => console.log(element.name?.lastName));
