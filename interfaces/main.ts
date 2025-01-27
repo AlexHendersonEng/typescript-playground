b124e54f4af5465ad5458cfa6080d3742ade15b8
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-27 19:00:22 +0000

    Added interfaces project

---
+++ interfaces/main.ts
@@ -0,0 +1,14 @@
+// Define interface
+interface Person {
+  name: string;
+  age: number;
+}
+
+// Create object
+const person: Person = {
+  name: 'John',
+  age: 30
+};
+
+// Print object
+console.log(person);
