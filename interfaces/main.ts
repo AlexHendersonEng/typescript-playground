ad82562a9acff33acf109b21a82bab9eddf332f3
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-28 19:17:51 +0000

    Updated interfaces project

--- interfaces/main.ts
+++ interfaces/main.ts
@@ -1,3 +1,9 @@
+/*
+    interface: A way to define the shape of an object by specifying the properties it must
+               have and their types. It acts as a contract, ensuring that objects adhere
+               to a specific structure.
+*/
+
 // Define interfaces
 interface Person {
   name: string;
@@ -9,7 +15,26 @@ interface Employee extends Person {
   salary: number;
 }
 
-// Create object
+// Define class which implements interface
+class SoftwareEngineer implements Employee {
+  name: string;
+  age: number;
+  salary: number;
+  favoriteLanguage: string;
+
+  constructor(name: string, age: number, salary: number, favoriteLanguage: string) {
+    this.name = name;
+    this.age = age;
+    this.salary = salary;
+    this.favoriteLanguage = favoriteLanguage;
+  }
+
+  hello() {
+    console.log('Hello', this.name);
+  }
+}
+
+// Create object using interface
 const person: Employee = {
   name: 'John',
   age: 30,
@@ -20,3 +45,10 @@ const person: Employee = {
 // Print object
 console.log(person);
 person.hello();
+
+// Create object using class
+const engineer = new SoftwareEngineer('John', 30, 1000, 'JavaScript');
+
+// Print object
+console.log(engineer);
+engineer.hello();
