718d723581a70a955417fdd981b883abef1ec323
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-29 19:38:03 +0000

    Added namespaces project

---
+++ namespaces/main.ts
@@ -0,0 +1,7 @@
+/*
+    namespace: A way to organise code into logical groups and avoid naming collisions
+               between identifiers in the global namespace.
+*/
+
+// Use 'hello' function from 'MyNamespace' defined in 'module.ts'
+MyNamespace.hello(); // Output: Hello World!
---
+++ namespaces/module.ts
@@ -0,0 +1,10 @@
+// Define namespace
+namespace MyNamespace { 
+    // Export function
+    export function hello() { 
+        console.log('Hello World!'); 
+    } 
+} 
+  
+// Output: This is my function in MyNamespace 
+MyNamespace.hello();
